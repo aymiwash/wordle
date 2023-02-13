57517b997da134432fa852cf8c52fae566696300
Emilie Luong <emi.luong92@gmail.com>
2023-02-13 18:56:11 +0100

    plus d'input

--- js_wordle.js
+++ js_wordle.js
@@ -1,42 +1,59 @@
-let letter = document.querySelector("form")
-let currentGuess = []
+let letter = ""
+let currentGuess = ["c", "a", "n", "n", "y"]
 let wordOfTheDay = "https://words.dev-apis.com/word-of-the-day"
+let validWordOrNot = "https://words.dev-apis.com/validate-word"
 
-
-async function whatIsTheWordToday(){
+//va chercher le mot du jour sur l'API
+async function whatIsTheWordToday() {
+    console.log("api word")
     let promise = await fetch(wordOfTheDay)
     let processedResponse = await promise.json()
     wordOfTheDay = processedResponse.word
-} //va chercher le mot du jour sur l'API
+}
 
-whatIsTheWordToday()
-
+//vérifie si input est UNE SEULE lettre de l'alphabet
 function isLetter(character) {
     return /^[a-zA-Z]$/.test(character);
-} //vérifie si input est UNE SEULE lettre de l'alphabet
+}
 
+//récupère la lettre tapée
 function init() {
-    letter.addEventListener("click", function (event) {
-        console.log(event.target.value)
-        letter = event.target.value
-        // console.log(letter)
+    console.log("init")
+    whatIsTheWordToday()
+    document.addEventListener("keydown", function (event) {
+        letter = event.key
+        if (isLetter(letter)) {
+            currentGuess.push(letter)
+        }
+        return letter
     }
     )
-} //récupère la lettre dans l'input
+    console.log(wordOfTheDay)
+}
+
 init()
 
-
-
-function nextLetter(currentInput) {
-if(isLetter(letter) && currentInput.length === 1) {
-    document.querySelector(".first.n2").focus()
-    currentGuess.push(letter)
-    
+async function doesWordExist(wordOfFiveChar) {
+    console.log("exist")
+    if (currentGuess.length === 5) {
+        let promise = await fetch(validWordOrNot, {
+            method: "POST",
+            body: JSON.stringify({ "word": wordOfFiveChar })
+        })
+        let processedResponse = await promise.json()
+        // console.log(processedResponse.validWord)
+        return processedResponse.validWord
+    }
 }
-}
 
-function isTheGuessGood(myActualGuess){
-    if( currentGuess.length === 5 && currentGuess.join("") === wordOfTheDay){
+
+//vérifie si le mot est juste, si oui, affiche "You Win">
+function isTheGuessGood(myActualGuess) {
+    console.log(myActualGuess)
+    console.log(wordOfTheDay)
+    if (myActualGuess.join("") === wordOfTheDay) {
         alert("You Win ! The word was", wordOfTheDay.toUpperCase())
     }
-}//vérifie si le mot est juste, si oui, affiche "You Win">
+}
+
+isTheGuessGood(currentGuess)
