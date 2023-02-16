dc80c439be0ee90e74e2a5bcf30f479c77c86a5d
Emilie Luong <emi.luong92@gmail.com>
2023-02-16 13:30:28 +0100

    maj wordle

--- js_wordle.js
+++ js_wordle.js
@@ -1,38 +1,32 @@
 let letter = ""
-let currentGuess = ["c", "a", "n", "n", "y"]
-let wordOfTheDay = "https://words.dev-apis.com/word-of-the-day"
+let currentGuess = ["s", "l", "u", "n", "g"]
+let wordOfTheDayAPI = "https://words.dev-apis.com/word-of-the-day"
 let validWordOrNot = "https://words.dev-apis.com/validate-word"
+let wordOfTheDay = ""
 
 //va chercher le mot du jour sur l'API
-async function whatIsTheWordToday() {
-    console.log("api word")
-    let promise = await fetch(wordOfTheDay)
-    let processedResponse = await promise.json()
-    wordOfTheDay = processedResponse.word
+function whatIsTheWordToday() {
+    let promise = fetch(wordOfTheDayAPI)
+    promise.then(function (response) {
+        let processingPromise = response.text();
+        return processingPromise;
+    })
+        .then(function (processedResponse) {
+            wordOfTheDay = JSON.parse(processedResponse).word
+            console.log(wordOfTheDay)
+            return wordOfTheDay
+        })
 }
+
+whatIsTheWordToday()
 
 //vérifie si input est UNE SEULE lettre de l'alphabet
 function isLetter(character) {
     return /^[a-zA-Z]$/.test(character);
 }
 
-//récupère la lettre tapée
-function init() {
-    console.log("init")
-    whatIsTheWordToday()
-    document.addEventListener("keydown", function (event) {
-        letter = event.key
-        if (isLetter(letter)) {
-            currentGuess.push(letter)
-        }
-        return letter
-    }
-    )
-    console.log(wordOfTheDay)
-}
 
-init()
-
+//vérifie si le mot fait 5 lettres et s'il existe
 async function doesWordExist(wordOfFiveChar) {
     console.log("exist")
     if (currentGuess.length === 5) {
@@ -49,11 +43,27 @@ async function doesWordExist(wordOfFiveChar) {
 
 //vérifie si le mot est juste, si oui, affiche "You Win">
 function isTheGuessGood(myActualGuess) {
-    console.log(myActualGuess)
-    console.log(wordOfTheDay)
     if (myActualGuess.join("") === wordOfTheDay) {
+        console.log("caca")
         alert("You Win ! The word was", wordOfTheDay.toUpperCase())
+
     }
+
 }
 
 isTheGuessGood(currentGuess)
+
+
+//récupère la lettre tapée
+function init() {
+    document.addEventListener("keydown", function (event) {
+        letter = event.key
+        if (isLetter(letter)) {
+            currentGuess.push(letter)
+        }
+        return letter
+    }
+    )
+}
+
+init()
